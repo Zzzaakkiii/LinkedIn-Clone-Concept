c9ff9b3cafb9066480a6b6251abb14b20d0f1108
Zaki <Zakibutt0207@gmail.com>
2021-11-18 10:49:56 +0500

    moved repo to personal github

---
+++ src/feed/controllers/feed.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtGuard } from 'src/auth/gaurds/jwt.guard';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { FeedPost } from '../models/post.interface';
+import { FeedService } from '../services/feed.service';
+
+@Controller('feed')
+export class FeedController {
+  constructor(private feedService: FeedService) {}
+
+  @UseGuards(JwtGuard)
+  @Post()
+  create(@Body() feedPost: FeedPost, @Request() req): Observable<FeedPost> {
+    return this.feedService.createPost(req.user, feedPost);
+  }
+
+  // @Get()
+  // findAll(): Observable<FeedPost[]> {
+  //   return this.feedService.findAllPosts();
+  // }
+
+  @Get()
+  findSelected(
+    @Query('take') take = 1,
+    @Query('skip') skip = 1,
+  ): Observable<FeedPost[]> {
+    take = take > 20 ? 20 : take;
+    return this.feedService.findPosts(take, skip);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() feedPost: FeedPost,
+  ): Observable<UpdateResult> {
+    return this.feedService.updatePost(id, feedPost);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.feedService.deletePost(id);
+  }
+}
